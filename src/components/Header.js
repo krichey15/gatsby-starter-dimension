f65037d2950a2b9bd2f96713c2d39a327d51b22a
krichey15 <richey.kyle.i@gmail.com>
2021-01-07 12:26:02 -0800

    header changes

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,17 +4,15 @@ import React from 'react'
 const Header = props => (
   <header id="header" style={props.timeout ? { display: 'none' } : {}}>
     <div className="logo">
-      <span className="icon fa-diamond"></span>
+      <img  />
     </div>
     <div className="content">
       <div className="inner">
-        <h1>Dimension</h1>
+        <h1>Kyle Richey</h1>
         <p>
-          A fully responsive site template designed by{' '}
-          <a href="https://html5up.net">HTML5 UP</a> and released
-          <br />
-          for free under the{' '}
-          <a href="https://html5up.net/license">Creative Commons</a> license.
+          Full-Stack JavaScript Developer
+          <br/>
+          React | NodeJS | MongoDB | SQL | AWS
         </p>
       </div>
     </div>
