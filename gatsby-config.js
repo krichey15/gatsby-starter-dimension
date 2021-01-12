b4609e4276262ec7569d261d02ea9aaffaaace93
krichey15 <richey.kyle.i@gmail.com>
2021-01-11 20:49:22 -0800

    gradient theme

--- gatsby-config.js
+++ gatsby-config.js
@@ -12,8 +12,8 @@ module.exports = {
         name: 'gatsby-starter-default',
         short_name: 'starter',
         start_url: '/',
-        background_color: '#663399',
-        theme_color: '#663399',
+        background_color: 'linear-gradient(to right, #40E0D0 0, #40E0D0 33%, #FF8C00 33%, #FF8C00 66%, #FF0080 66%, #FF0080 100%);',
+        theme_color: 'linear-gradient(to right, #40E0D0 0, #40E0D0 33%, #FF8C00 33%, #FF8C00 66%, #FF0080 66%, #FF0080 100%);',
         display: 'minimal-ui',
         icon: 'src/images/kr-icon.png', // This path is relative to the root of the site.
       },
